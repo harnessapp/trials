e450e2c97a4c67f2b253b791d978456cccb677de
joel <joelpatching@gmail.com>
2026-03-17 20:50:32 +1100

    Update trials site: latest changes

---
+++ update_trials_site.bat
@@ -0,0 +1,17 @@
+@echo off
+cd C:\trials
+
+echo Running export_trials_json...
+python C:\harness_scraper\harness_api\export_trials_json.py --input C:\Users\joel\FlutterProjects\harness_app\assets\upcoming_fields.csv --output C:\trials\data\trials.json
+
+echo Adding changes...
+git add .
+
+echo Committing changes...
+git commit -m "Update trials site: latest changes"
+
+echo Pushing to GitHub...
+git push origin main
+
+echo Done! Press any key to exit.
+pause
